2da7056995c4c2870ae1dd875550372992741013
unknown <qui7d18@gmail.com>
2025-10-27 20:30:57 +0700

    update

--- index.js
+++ index.js
@@ -153,7 +153,36 @@ async function CheckBalanceStellarQuestReward(url, arrayReward) {
             }
 
             if (!allMatch) {
-                sendNotification("Stellar Quest reward giam.");
+                sendNotification("Stellar REWARD giam.");
+                statusReturn = true;
+            }
+        });
+
+        return statusReturn;
+    } catch (error) {
+        console.error('Lỗi khi lấy HTML:', error);
+    }
+}
+async function CheckBalanceBobaQuestReward(url, arrayReward) {
+    try {
+        let statusReturn = false;
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        const parentDiv = $('ul.grid.grid-cols-1.md\\:grid-cols-2.lg\\:grid-cols-3.gap-3');
+
+        parentDiv.children().each((i, child) => {
+            const whiteTextEl = $(child).find('.text-white'); // tìm thẻ có class text-white bên trong child
+            const text = whiteTextEl.text().trim();
+            // console.log("In: ", text)
+            let allMatch = true;
+            // console.log('Link text:', text);
+            if (!arrayReward.includes(text)) {
+                if (text == "4000 BOBA" || text == "6000 BOBA" || Number(text.split(" ")[0]) > 6000)
+                    allMatch = false;
+            }
+
+            if (!allMatch) {
+                sendNotification("REWAR BOBA DUOC BOM...");
                 statusReturn = true;
             }
         });
@@ -164,7 +193,57 @@ async function CheckBalanceStellarQuestReward(url, arrayReward) {
     }
 }
 
+// need to fix
+async function JourneysBoba(url) {
+    let statusReturn = false;
+    try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        const parentDiv = $("ul.grid.grid-cols-1.md\\:grid-cols-2.lg\\:grid-cols-3.gap-3");
+        console.log(parentDiv)
+        parentDiv.children().each((i, child) => {
+            console.log(1)
+        }) 
 
+    } catch (error) {
+        console.error('Lỗi khi lấy HTML:', error);
+    }
+};
+async function QuestRewardBoba2(url) {
+    try {
+        let statusReturn = false;
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        const parentDiv = $('div.grid.grid-cols-4.gap-6.py-6');
+        if (parentDiv.children().length > 4) {
+            sendNotification("Boba Quest Rewards.");
+            statusReturn = true;
+        } else {
+            const matchText = [
+                'Bridge from Base to Boba via Symbiosis',
+                'Deposit ETH with RubyScore',
+                'Open position on Lynx - BOBA',
+            ];
+            parentDiv.children().each((i, child) => {
+                const text = $(child).find('a').eq(1).text().trim();
+                let allMatch = true;
+                // console.log('Link text:', text);
+                if (!matchText.includes(text)) {
+                    allMatch = false; // Có ít nhất 1 cái không khớp
+                }
+                // }
+
+                if (!allMatch) {
+                    sendNotification("Stellar Quest Rewards2.");
+                    statusReturn = true;
+                }
+            });
+        }
+        return statusReturn;
+    } catch (error) {
+        console.error('Lỗi khi lấy HTML:', error);
+    }
+}
 async function JourneysStellar(url) {
     let statusReturn = false;
     try {
@@ -198,17 +277,23 @@ app.get('/', async (req, res) => {
 app.get('/check', async (req, res) => {
     try {
         // let statusReturn1 = await QuestRewardAptos("https://flipsidecrypto.xyz/earn/aptos");
-        let statusReturn2 =await QuestRewardStellar2("https://flipsidecrypto.xyz/earn/stellar");
-        let statusReturn3 =await QuestRewardNear("https://flipsidecrypto.xyz/earn/near");
+        let statusReturn2 = await QuestRewardStellar2("https://flipsidecrypto.xyz/earn/stellar");
+        let statusReturn3 = await QuestRewardNear("https://flipsidecrypto.xyz/earn/near");
         // let statusReturn4 = await CheckBalanceStellarQuestReward("https://flipsidecrypto.xyz/earn/quest/lend-on-blend-yieldblox-pool", ['200 USDC', '1200 USDC', '750 USDC']);
         // let statusReturn5 = await CheckBalanceStellarQuestReward("https://flipsidecrypto.xyz/earn/quest/borrow-on-blend-yieldblox-v2-pool", ['200 USDC', '1200 USDC', '1500 USDC']);
         // let statusReturn6 = await CheckBalanceStellarQuestReward("https://flipsidecrypto.xyz/earn/quest/swap-usdc-for-pho-on-phoenix", ['100 USDC', '0 USDC', '0 USDC']);
         // let statusReturn7 = await CheckBalanceStellarQuestReward("https://flipsidecrypto.xyz/earn/quest/provide-liquidity-to-pho-usdc-on-phoenix", ['100 USDC', '0 USDC', '0 USDC']);
 
-        if (statusReturn2||statusReturn3) {
+        let statusReturn8 = await CheckBalanceBobaQuestReward("https://flipsidecrypto.xyz/earn/quest/open-position-on-lynx-lvkqhn", ['0 BOBA', '5547.9732197094 BOBA', '0 BOBA']);
+        let statusReturn9 = await CheckBalanceBobaQuestReward("https://flipsidecrypto.xyz/earn/quest/deposit-eth-with-rubyscore", ['0 BOBA', '1154.3756858666 BOBA', '0 BOBA']);
+        // let statusReturn10 = await JourneysBoba("https://flipsidecrypto.xyz/earn/journey/boba-bridge-lp-journey-d3bCh");
+        let statusReturn10 = await QuestRewardBoba2("https://flipsidecrypto.xyz/earn/boba");
+
+        if (statusReturn2 || statusReturn3 || statusReturn8 || statusReturn9 || statusReturn10) {
             setInterval(() => {
                 sendNotification("+++++LAM VIEC THOI+++++.");
-              }, 5000)
+            }, 5000)
+            console.log("RUNING...")
         }
     } catch (error) {
         sendNotification("----ERROR CALLING----")
